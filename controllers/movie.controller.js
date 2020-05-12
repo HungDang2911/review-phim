0cecb14095e4fa78f2a98952c1d2eb7ef247822c
HungDang2911 <53470092+HungDang2911@users.noreply.github.com>
2020-05-12 16:07:35 +0700

    Update movie.controller.js

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -13,11 +13,11 @@ module.exports.getMovieById = (req, res) => {
         });
     })
 
-    const movie = await connection.execute('SELECT `movieName`,`genre`,`releaseDate`,`movieTime`,`trailerLink`,`posterLink`,`imdb`,`rottenTomatoes`,`metacritic`,`movieDescription` FROM `movies` WHERE `movieId` = (?)', [movieId]);
-    const actors = await connection.execute('SELECT actors.actorId,`actorName`,`imageLink` FROM `actors` LEFT JOIN `movies_actors` ON actors.actorId = movies_actors.actorId WHERE movies_actors.movieId = (?)', [movieId]);
-    const directors = await connection.execute('SELECT directors.directorId,`directorName`,`imageLink` FROM `directors` LEFT JOIN `movies_directors` ON directors.directorId = movies_directors.directorId WHERE movies_directors.movieId = (?)', [movieId]);
-    console.log({movie, actors, directors});
-    res.render('movie', {movie, actors, directors});
+    // const movie = await connection.execute('SELECT `movieName`,`genre`,`releaseDate`,`movieTime`,`trailerLink`,`posterLink`,`imdb`,`rottenTomatoes`,`metacritic`,`movieDescription` FROM `movies` WHERE `movieId` = (?)', [movieId]);
+    // const actors = await connection.execute('SELECT actors.actorId,`actorName`,`imageLink` FROM `actors` LEFT JOIN `movies_actors` ON actors.actorId = movies_actors.actorId WHERE movies_actors.movieId = (?)', [movieId]);
+    // const directors = await connection.execute('SELECT directors.directorId,`directorName`,`imageLink` FROM `directors` LEFT JOIN `movies_directors` ON directors.directorId = movies_directors.directorId WHERE movies_directors.movieId = (?)', [movieId]);
+    // console.log({movie, actors, directors});
+    // res.render('movie', {movie, actors, directors});
 };
 
 module.exports.postCommentAboutMovie = (req, res) => {
